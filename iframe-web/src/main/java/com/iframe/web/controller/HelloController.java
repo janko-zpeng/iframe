c82534aad76a7fc62cc5634f6e145a18cf8e9078
peng <tpkey@qq.com>
2017-11-19 23:47:07 +0800

    init

---
+++ iframe-web/src/main/java/com/iframe/web/controller/HelloController.java
@@ -0,0 +1,36 @@
+package com.iframe.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author janko
+ * @date 18/11/2017
+ * @desc
+ */
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+
+	@RequestMapping(value = "helloworld.vpage", method = {RequestMethod.GET})
+	@ResponseBody
+	public String helloworld() {
+
+		return "hello world";
+	}
+
+	@RequestMapping(value = "index.vpage", method = {RequestMethod.GET})
+	public String index() {
+
+		return "index";
+	}
+
+	@RequestMapping(method = {RequestMethod.GET})
+	public String home() {
+
+		return "home";
+	}
+}
